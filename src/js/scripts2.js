7c28d2a89cdf78761a7a1371877d1c80518ccaad
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-02-22 22:49:58 -0500

    Added search bar initial functionality as well as 4 submodals

--- src/js/scripts2.js
+++ src/js/scripts2.js
@@ -25,6 +25,8 @@ function addListItem(hero) {
 
   let listItem = document.createElement("li");
   listItem.classList.add("card");
+  listItem.setAttribute("data-name", `${hero.name}`);
+  // listItem.classList.add("list-group-item");
   heroesList.appendChild(listItem);
   listItem.innerHTML = `<img class="card-image" src="${hero.image.url}"><button type="button" class="card-button" id="hero-name" class="btn" data-toggle="modal"
   data-target="#exampleModal">${hero.name}</button>
@@ -74,7 +76,7 @@ function showModal(hero) {
   //   closeButton.innerHTML = `<button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="hideModal()"><span aria-hidden="true">&times;</span></button>`;
   //   modal.appendChild(closeButton);
   modalImageContainer.innerHTML = `<img class="modal-image" src="${hero.image.url}"/>`;
-  modalFooter.innerHTML = `<button type="button" onclick="showPowers()" class="powers-button">Powers</button> <button type="button" class="btn btn-secondary"data-dismiss="modal"
+  modalFooter.innerHTML = `<button type="button" onclick="showPowers()" class="powers-button btn btn-secondary ">Powers</button><button type="button" onclick="showAppearance()" class="appearance-button btn btn-secondary ">Appearance</button> <button type="button" onclick="showBio()" class="bio-button btn btn-secondary ">Bio</button><button type="button" onclick="showAffiliations()" class="affiliations-button btn btn-secondary ">Work</button><button type="button" class="btn btn-secondary"
   onclick="hideModal()">Close</button>`;
   localStorage.setItem("hero", JSON.stringify(hero));
 }
@@ -88,9 +90,15 @@ function hideModal() {
 }
 
 function showPowers() {
+  document.querySelector(".powers-button").disabled = true;
+
   let powersArray = [];
   let powersModal = document.querySelector(".powers-modal");
+  // powersModal.setAttribute("onclick", "console.log('hello')");
   powersModal.classList.remove("hidden");
+  let powersTitle = document.createElement("h2");
+  powersTitle.innerText = "Power Stats";
+  powersModal.appendChild(powersTitle);
   let powersList = document.createElement("ul");
   powersModal.appendChild(powersList);
   let character = JSON.parse(localStorage.getItem("hero"));
@@ -100,29 +108,128 @@ function showPowers() {
     powersArray.push(power);
     console.log(powersArray);
   }
+  powersList.innerHTML = powersArray.join(" ");
+
   let closeModalButton = document.createElement("button");
   closeModalButton.innerHTML = `<span>&times;</span>`;
-  closeModalButton.classList.add("close-modal")
-  closeModalButton.setAttribute("onclick", "closeModal()");
+  closeModalButton.classList.add("close-modal");
+  closeModalButton.setAttribute("onclick", "closePowersModal()");
   powersModal.appendChild(closeModalButton);
-  powersList.innerHTML = powersArray.join(" ");
 
   // powersList.appendChild(newPowersArray);
 }
 
-function closeModal(){
-  console.log('clicked close modal')
+function closePowersModal() {
+  console.log("clicked close Powers modal");
+  let powersModal = document.querySelector(".powers-modal");
+  powersModal.classList.add("hidden");
+  document.querySelector(".powers-button").disabled = false;
 }
 
+function showAppearance() {
+  document.querySelector(".appearance-button").disabled = true;
 
+  let appearanceArray = [];
+  let appearanceModal = document.querySelector(".appearance-modal");
+  // powersModal.setAttribute("onclick", "console.log('hello')");
+  appearanceModal.classList.remove("hidden");
+  let appearanceTitle = document.createElement("h2");
+  appearanceTitle.innerText = "Appearance";
+  appearanceModal.appendChild(appearanceTitle);
+  let appearanceList = document.createElement("ul");
+  appearanceModal.appendChild(appearanceList);
+  let character = JSON.parse(localStorage.getItem("hero"));
+  console.log(character.appearance);
+  for (const [key, value] of Object.entries(character.appearance)) {
+    let appearance = `<li>${key}: ${value}</li>`;
+    appearanceArray.push(appearance);
+    console.log(appearanceArray);
+  }
+  appearanceList.innerHTML = appearanceArray.join(" ");
 
+  let closeModalButton = document.createElement("button");
+  closeModalButton.innerHTML = `<span>&times;</span>`;
+  closeModalButton.classList.add("close-modal");
+  closeModalButton.setAttribute("onclick", "closeAppearanceModal()");
+  appearanceModal.appendChild(closeModalButton);
+}
 
+function closeAppearanceModal() {
+  console.log("clicked close Appearance modal");
+  let appearanceModal = document.querySelector(".appearance-modal");
+  appearanceModal.classList.add("hidden");
+  document.querySelector(".appearance-button").disabled = false;
+}
 
+function showBio() {
+  document.querySelector(".bio-button").disabled = true;
 
+  let bioArray = [];
+  let bioModal = document.querySelector(".bio-modal");
+  // powersModal.setAttribute("onclick", "console.log('hello')");
+  bioModal.classList.remove("hidden");
+  let bioTitle = document.createElement("h2");
+  bioTitle.innerText = "Biography";
+  bioModal.appendChild(bioTitle);
+  let bioList = document.createElement("ul");
+  bioModal.appendChild(bioList);
+  let character = JSON.parse(localStorage.getItem("hero"));
+  console.log(character.biography);
+  for (const [key, value] of Object.entries(character.biography)) {
+    let bio = `<li>${key}: ${value}</li>`;
+    bioArray.push(bio);
+    console.log(bioArray);
+  }
+  bioList.innerHTML = bioArray.join(" ");
 
+  let closeModalButton = document.createElement("button");
+  closeModalButton.innerHTML = `<span>&times;</span>`;
+  closeModalButton.classList.add("close-modal");
+  closeModalButton.setAttribute("onclick", "closeBioModal()");
+  bioModal.appendChild(closeModalButton);
+}
 
+function closeBioModal() {
+  console.log("clicked close Bio modal");
+  let bioModal = document.querySelector(".bio-modal");
+  bioModal.classList.add("hidden");
+  document.querySelector(".bio-button").disabled = false;
+}
 
+function showAffiliations() {
+  document.querySelector(".affiliations-button").disabled = true;
 
+  let affiliationsArray = [];
+  let affiliationsModal = document.querySelector(".affiliations-modal");
+  // powersModal.setAttribute("onclick", "console.log('hello')");
+  affiliationsModal.classList.remove("hidden");
+  let affiliationsTitle = document.createElement("h2");
+  affiliationsTitle.innerText = "Work";
+  affiliationsModal.appendChild(affiliationsTitle);
+  let affiliationsList = document.createElement("ul");
+  affiliationsModal.appendChild(affiliationsList);
+  let character = JSON.parse(localStorage.getItem("hero"));
+  console.log(character.work);
+  for (const [key, value] of Object.entries(character.work)) {
+    let affiliation = `<li>${key}: ${value}</li>`;
+    affiliationsArray.push(affiliation);
+    console.log(affiliationsArray);
+  }
+  affiliationsList.innerHTML = affiliationsArray.join(" ");
+
+  let closeModalButton = document.createElement("button");
+  closeModalButton.innerHTML = `<span>&times;</span>`;
+  closeModalButton.classList.add("close-modal");
+  closeModalButton.setAttribute("onclick", "closeAffiliationsModal()");
+  affiliationsModal.appendChild(closeModalButton);
+}
+
+function closeAffilationsModal() {
+  console.log("clicked close Affiliations modal");
+  let affiliationsModal = document.querySelector(".affiliations-modal");
+  affiliationsModal.classList.add("hidden");
+  document.querySelector(".affiliations-button").disabled = false;
+}
 
 window.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
@@ -132,33 +239,36 @@ window.addEventListener("keydown", function (e) {
 
 window.addEventListener("load", getHeroesInfo());
 
-//   return {
-//     getAll: getAll,
-//     add: add,
-//     // addListItem: addListItem,
-//     loadHeroIdByName: loadHeroIdByName,
-//     // loadDetails: loadDetails,
-//     // showModal: showModal,
-//     // hideModal: hideModal,
-//     // showDetails: showDetails,
-//   };
+function search() {
+  let nav = document.querySelector(".nav");
+  nav.addEventListener("click", () => {
+    let search = document.querySelector("#heroes-search");
+    let li = Array.from(document.querySelectorAll(".card"));
+    console.log(li);
+    search.addEventListener("keyup", () => {
+      console.log(search.value);
+      value = search.value;
 
-// Search for specific pokemon
-// $(document).ready(function(){
-//   $('#pokemon-search').on('keyup', function() {
-//     let value = $(this).val().toLowerCase();
-//     $('.list-group-item').filter(function() {
-//       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-//     });
-//   });
-// });
+      for (i = 0; i < li.length; i++) {
+        txtAttrib = li[i].getAttribute("data-name");
+        console.log(txtAttrib);
+        if (txtAttrib.toLowerCase().indexOf(value) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
 
-// heroesRepository.loadList().then(function () {
-//   heroesRepository.getAll().forEach(function (hero) {
-//     heroesRepository.addListItem(hero);
-//   });
-// });
+      // value = this.value.toLowerCase();
+      // let heroesList = document.querySelector(".heroes-list");
+      // let listGroup = document.querySelectorAll(".list-group-item");
+      // let listItems = Array.from(listGroup);
+      // listItems.filter(function () {
+      //   console.log(this);
+      // this.ontoggle(this.innerText.toLowerCase().indexOf(value) > -1);
+      // });
+    });
+  });
+}
 
-// heroesRepository.loadHeroIdByName().then(() => {
-//   console.log(hero);
-// });
+search();
